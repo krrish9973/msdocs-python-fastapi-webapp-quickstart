c272cc7e68ce40dc4fa20133e98cd4df1e47c295
Krishna Joshi <er.krishna12@gmail.com>
2024-11-08 21:49:54 +0100

    Api Issue1

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_cors import CORS
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')  # Specify the template folder
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 @app.route("/api")
-def api():
-    return {"message": "Hello from API!"}
+def root():
+    return render_template('api.json')
 
-if __name__ == "__main__":
-    app.run(debug=True)  # Not necessary in production, but useful for debugging locally
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=80)
